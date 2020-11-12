99cc34ac8d650d400db11fa5ae830715bb7c4b39
Wai-Yan-Htoo <66287776+Wai-Yan-Htoo@users.noreply.github.com>
2020-11-11 22:33:12 -1100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace Movie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_title = new System.Windows.Forms.TextBox();
+            this.txt_release_date = new System.Windows.Forms.TextBox();
+            this.txt_genre = new System.Windows.Forms.TextBox();
+            this.txt_price = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dgtitle = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgreleasedate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dg_genre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dg_price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(83, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Release Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(75, 138);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Genre";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(85, 185);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 24);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Price";
+            // 
+            // txt_title
+            // 
+            this.txt_title.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_title.Location = new System.Drawing.Point(166, 53);
+            this.txt_title.Name = "txt_title";
+            this.txt_title.Size = new System.Drawing.Size(135, 26);
+            this.txt_title.TabIndex = 4;
+            // 
+            // txt_release_date
+            // 
+            this.txt_release_date.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_release_date.Location = new System.Drawing.Point(166, 94);
+            this.txt_release_date.Name = "txt_release_date";
+            this.txt_release_date.Size = new System.Drawing.Size(135, 26);
+            this.txt_release_date.TabIndex = 5;
+            // 
+            // txt_genre
+            // 
+            this.txt_genre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_genre.Location = new System.Drawing.Point(166, 138);
+            this.txt_genre.Name = "txt_genre";
+            this.txt_genre.Size = new System.Drawing.Size(135, 26);
+            this.txt_genre.TabIndex = 6;
+            // 
+            // txt_price
+            // 
+            this.txt_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_price.Location = new System.Drawing.Point(166, 183);
+            this.txt_price.Name = "txt_price";
+            this.txt_price.Size = new System.Drawing.Size(135, 26);
+            this.txt_price.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(134, 250);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 28);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(273, 250);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(105, 28);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(134, 302);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 28);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Show Data";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgtitle,
+            this.dgreleasedate,
+            this.dg_genre,
+            this.dg_price});
+            this.dataGridView1.Location = new System.Drawing.Point(460, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(446, 156);
+            this.dataGridView1.TabIndex = 11;
+            // 
+            // dgtitle
+            // 
+            this.dgtitle.HeaderText = "Title";
+            this.dgtitle.Name = "dgtitle";
+            // 
+            // dgreleasedate
+            // 
+            this.dgreleasedate.HeaderText = "Release Date";
+            this.dgreleasedate.Name = "dgreleasedate";
+            // 
+            // dg_genre
+            // 
+            this.dg_genre.HeaderText = "Genre";
+            this.dg_genre.Name = "dg_genre";
+            // 
+            // dg_price
+            // 
+            this.dg_price.HeaderText = "Price";
+            this.dg_price.Name = "dg_price";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(948, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txt_price);
+            this.Controls.Add(this.txt_genre);
+            this.Controls.Add(this.txt_release_date);
+            this.Controls.Add(this.txt_title);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_title;
+        private System.Windows.Forms.TextBox txt_release_date;
+        private System.Windows.Forms.TextBox txt_genre;
+        private System.Windows.Forms.TextBox txt_price;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgtitle;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgreleasedate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dg_genre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dg_price;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Movie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //---------------------------------------
+        void cleartextbox()
+        {
+            txt_title.Clear();
+            txt_release_date.Clear();
+            txt_genre.Clear();
+            txt_price.Clear();
+        }
+        //---------------------------------------------
+        /// <summary>
+        /// global variable for use every function
+        /// </summary>
+        string connectionstring;
+        MySqlConnection conn;
+        MySqlCommand command;
+        //--------------------------------------------
+        void selectfunction()
+        {
+            connectionstring = string.Format("server=localhost;uid=root;pwd=;database=db_flim");
+
+            conn = new MySqlConnection(connectionstring);
+
+            conn.Open();
+            string query = "select * from movies";
+            command = new MySqlCommand(query, conn);
+            
+            MySqlDataReader reader = command.ExecuteReader();
+            string output = "";
+            while (reader.Read())
+            {
+                output = output + reader.GetValue(0) + " , " + reader.GetValue(1) + " , " + reader.GetValue(2) + " , " + reader.GetValue(3)+"\n";
+            }
+            MessageBox.Show(output);
+            reader.Close();
+            command.Dispose();
+            
+
+
+        }
+
+        //---------------------------------------------
+        void insertfunction()
+        {
+            string title, releasedate, genre;
+            int price;
+            title = txt_title.Text;
+            releasedate = txt_release_date.Text;
+            genre = txt_genre.Text;
+            price = int.Parse(txt_price.Text);
+
+            connectionstring = string.Format("server=localhost;uid=root;pwd=;database=db_flim");
+
+            conn = new MySqlConnection(connectionstring);
+
+            conn.Open();
+            string query = "insert into movies values('" + title + "','" + releasedate + "','" + genre + "','" + price + "')";
+            command = new MySqlCommand(query, conn);
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            adapter.InsertCommand = new MySqlCommand(query, conn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            MessageBox.Show("data success");
+            cleartextbox();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            insertfunction();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            selectfunction();
+        }
+    }
+}
